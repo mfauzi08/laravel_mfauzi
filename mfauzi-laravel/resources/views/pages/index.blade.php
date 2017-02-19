1a27ccf325a692d181220fcabe4bcd1bdcd08727
mfauzi08 <mister.oz17@gmail.com>
2017-02-19 14:38:05 +0700

    Uts

---
+++ mfauzi-laravel/resources/views/pages/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.default')
+@section('content')
+<form action="" method="post">
+	<table>
+		<tr>
+			<td><label>Username :</label></td>
+			<td><input id="name" name="username" placeholder="username" type="text"></td>
+		</tr>
+		<tr>
+			<td><label>Password :</label></td>
+			<td><input id="password" name="password" placeholder="**********" type="password"></td>
+		</tr>
+		<tr colspan="2">
+			<td><input name="login" type="submit" value=" Login "></td>
+		</tr>
+	</table>
+</form>
+@stop
