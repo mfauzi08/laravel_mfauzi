5f1cf1e1d404103e1d07dc46e4ba1f7bc0b4a6e1
mfauzi08 <mister.oz17@gmail.com>
2017-02-19 19:26:34 +0700

    tgs_controller

---
+++ mfauzi-laravel2/resources/views/templates/index.blade.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Latihan Blade Templates</title>
+	</head>
+	
+	<body>
+		@include('templates.header')
+		@yield('content')
+		@include('templates.footer')
+	</body>
+</html>
